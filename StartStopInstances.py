7ecbc95b89aa09aef362de3c75c9db1bc579658d
Bijohn Vincent <bijohnvincent@users.noreply.github.com>
2016-08-31 20:40:41 +0530

    Added option for scheduling
    -Added more options in scheduling
    -Improvements

--- StartStopInstances.py
+++ StartStopInstances.py
@@ -8,13 +8,13 @@
 #   }
 #
 #       Based on the action (Not case sensitive), function will start or stop the instances specified in the payload.
-#       Start or stop function is restricted to instances having a Tag 'Start-StopHour' for more control and 
+#       Start or stop function is restricted to instances having a Tag 'Start-StopHourUTC' for more control and 
 #           unintentional stop or start of instances.
 #
-# (2) If there is no payload, it will check value of tag 'Start-StopHour' and will start or stop instances based on the time specified.
+# (2) If there is no payload, it will check value of tag 'Start-StopHourUTC' and will start or stop instances based on the time specified.
 #       This option is schedule based. Time will be read in UTC.
-#       Expected tag value is : start:HH-stop:HH or start:HH or stop:HH
-#       Both fields are not mandatory. You can use any one of the action too.
+#       Expected tag value is : start:HH-stop:HH or start:HH or stop:HH or None
+#       Both start and stop fields are not mandatory. You can use any one of the action as shown above.
 #       If the servers are started or stopped on invocation only, just add 'none' as value. (False, 0, None, No & N will also work)
 #
 
@@ -34,13 +34,12 @@ currentHour = now.hour
 
 #
 # This function starts the instances  passed as parameter
-# Comma separated Instance Name tag is the expected value
+# Python list with instance names is the expected value
 #
 def StartInstances(instanceNames):
-    instanceNameList = instanceNames.replace(" ", "").split(",")
-    
+
     # Get details of the instances that has name as in input parameter
-    Reservations = ec2.describe_instances(Filters=[{'Name':'tag:Name', 'Values':instanceNameList}])
+    Reservations = ec2.describe_instances(Filters=[{'Name':'tag:Name', 'Values':instanceNames}])
     
     for Instances in Reservations['Reservations']:
         for Instance in Instances['Instances']:
@@ -50,11 +49,10 @@ def StartInstances(instanceNames):
             # Start the instance only if it is in stopped state
             if Instance['State']['Name'] == 'stopped':
                 
-                # Ensures only instances with tag 'Start-StopHour' can be started
-                for tag in Instance['Tags']:
-                    if tag['Key'] == 'Start-StopHour':
-                        skipStart=False
-                
+                # Ensures only instances with tag 'Start-StopHourUTC' can be started
+                if 'Start-StopHourUTC' in str(Instance['Tags']):
+                    skipStop=False
+                    
                 # starts the instance
                 if not skipStart:
                     print "Starting %s with ip %s" %(Instance['InstanceId'], Instance['PrivateIpAddress'])
@@ -65,7 +63,7 @@ def StartInstances(instanceNames):
                     print response
                 
                 else:
-                    print "Not permitted to start servers without 'Start-StopHour' tag"
+                    print "Not permitted to start servers without 'Start-StopHourUTC' tag"
             else:
                 print Instance['PrivateIpAddress'] + " is not in stopped state" 
     return
@@ -74,26 +72,24 @@ def StartInstances(instanceNames):
 
 #
 # This function will stop the instances passed as parameter
-# Comma separated Instance Name tag is the expected value
+# Python list with instance names is the expected value
 #
 def StopInstances(instanceNames):
-    instanceNameList = instanceNames.replace(" ", "").split(",")
-    
+
     # Get details of the instances that has name as in input parameter
-    Reservations = ec2.describe_instances(Filters=[{'Name':'tag:Name', 'Values':instanceNameList}])
+    Reservations = ec2.describe_instances(Filters=[{'Name':'tag:Name', 'Values':instanceNames}])
     
     for Instances in Reservations['Reservations']:
         for Instance in Instances['Instances']:
             skipStop=True
             #print Instance['InstanceId'], Instance['State']['Name']
             
-            # Start the instance only if it is in running state
+            # Stop the instance only if it is in running state
             if Instance['State']['Name'] == 'running':
                 
-                # Ensures only instances with tag 'Start-StopHour' can be stopped
-                for tag in Instance['Tags']:
-                    if tag['Key'] == 'Start-StopHour':
-                        skipStop=False
+                # Ensures only instances with tag 'Start-StopHourUTC' can be stopped
+                if 'Start-StopHourUTC' in str(Instance['Tags']):
+                    skipStop=False
                 
                 # stops the instance
                 if not skipStop:
@@ -105,7 +101,7 @@ def StopInstances(instanceNames):
                     print response
                 
                 else:
-                    print "Not permitted to stop servers without 'Start-StopHour' tag."
+                    print "Not permitted to stop servers without 'Start-StopHourUTC' tag."
             else:
                 print Instance['PrivateIpAddress'] + " is not in running state" 
     return
@@ -114,14 +110,13 @@ def StopInstances(instanceNames):
 
 
 #
-# This function will red the tag Start-StopHour and will take necessary action 
+# This function will read the tag Start-StopHourUTC and will take necessary action 
 # by calling StartInstances() or StopInstances()
 #
 def CheckTagsAndTakeAction():
-    print "No Payload"
-    
+
     # Filter instances that has specific Tag-key
-    Reservations = ec2.describe_instances(Filters=[{'Name':'tag-key','Values':['Start-StopHour']}])
+    Reservations = ec2.describe_instances(Filters=[{'Name':'tag-key','Values':['Start-StopHourUTC']}])
     
     # Initialize name of instances to be started or stopped.
     instanceToStart = ""
@@ -129,9 +124,8 @@ def CheckTagsAndTakeAction():
     
     for Instances in Reservations['Reservations']:
         for Instance in Instances['Instances']:
-            #print Instance
+            print Instance['PrivateIpAddress'] + ":"
             skipAction=False   # Initilize skipAction for each instances
-            
             
             # Process the Tags
             for tag in Instance['Tags']:
@@ -144,7 +138,7 @@ def CheckTagsAndTakeAction():
                 # Check if the tag is value is a negation. 
                 # If yes, skip start or stop action.
                 # Else split tag value with '-' as delimiter
-                if tag['Key'] == 'Start-StopHour':
+                if tag['Key'] == 'Start-StopHourUTC':
                     StartStopHourList = []
                     StartStopHour = tag['Value'].replace(' ', '').lower()
                     print StartStopHour
@@ -155,7 +149,7 @@ def CheckTagsAndTakeAction():
                     else:
                         if ':' in StartStopHour:
                             StartStopHourList = StartStopHour.split("-")
-                            print StartStopHourList
+                            #print StartStopHourList
                         else:
                             print "Wrong format. Expected format start:HH-stop:HH or start:HH or stop:HH"
                             skipAction=True
@@ -171,21 +165,21 @@ def CheckTagsAndTakeAction():
                             instanceToStart = instanceToStart + ","+ instanceName
                     elif 'stop' in action_time.lower():
                         if int(action_time.split(':')[1]) == currentHour:
-                            print "Append to stop"
                             instanceToStop = instanceToStop + ","+ instanceName
                     else:
                         print "Wrong action. Should be either 'start' or 'stop'."
     
-    
+    # Convert the comma separated instance names to a pythin list
+    instanceToStart = instanceToStart.lstrip(',').split(',')
+    instanceToStop = instanceToStop.lstrip(',').split(',')
+
     # If length of 'instanceToStart' or 'instanceToStop' variabble is greater
     # than 0, call curresponding function to start or stop the instances.
-    instanceToStart = instanceToStart.lstrip(',')
-    instanceToStop = instanceToStop.lstrip(',')
-    if  len(instanceToStart) > 0:
-        print "Statring : " + instanceToStart
+    if not instanceToStart == ['']:
+        print "Statring : " + str(instanceToStart)
         StartInstances(instanceToStart)
-    if len(instanceToStop) > 0:
-        print "Stopping : " + instanceToStop
+    if not instanceToStop == ['']:
+        print "Stopping : " + str(instanceToStop)
         StopInstances(instanceToStop)
     return    
     
@@ -194,10 +188,12 @@ def CheckTagsAndTakeAction():
 # This is the main function 
 #
 def start_stop(json_val, context):
+    #print json_val
+    print "current hour(UTC) : " + str(currentHour)
     
-    # If an input is given, peocess the json file and take necessary action
-    if json_val:
-        
+    # If not invoked by CloudWatch event, process the json input and take necessary action
+    if not 'Scheduled Event' in str(json_val):
+        print "Manual invocation"
         
         # Validate the input json file print expected format if the json is in wrong format.
         if not 'action' in json_val or not 'instances' in json_val:
@@ -208,17 +204,20 @@ def start_stop(json_val, context):
                    }'''
             return None
         
+        # Convert 'instances' in json to python 'list'. Remove unwanted spaces in the list.
+        instances=map(str.strip,str(json_val['instances']).split(","))
         
         # Call correct function based on action 'start' or 'stop'
         if json_val['action'].lower() == 'start':
-            StartInstances(json_val['instances'])
+            StartInstances(instances)
         elif json_val['action'].lower() == 'stop':
-            StopInstances(json_val['instances'])
+            StopInstances(instances)
         else:
             print json_val['action'] + " is not a valid action. Action can be either 'start' or 'stop'"
     
     
-    # If no input is provided, call function for checking tag and take action according to tag
+    # If invoked by CloudWatch Scheduled Event, call function for checking tag and take action according to tag
     else:
+        print "CloudWatch Scheduled event"
         CheckTagsAndTakeAction()
     return
